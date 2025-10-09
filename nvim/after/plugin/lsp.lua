21ffb64095a3d099c25d7190d6399582dcae8593
Emma <148833230+plumbey@users.noreply.github.com>
2025-10-09 14:15:59 -0700

    update nvim lsp stuff

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,1 +1,10 @@
 vim.lsp.enable('clangd')
+
+vim.lsp.config('lua_ls', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {'vim'}}}}})
+
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('rust_analyzer')
