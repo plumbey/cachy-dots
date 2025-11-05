37c6b30cae3d4ac01b09fb1ffd8ba020546d1ac3
Emma <148833230+plumbey@users.noreply.github.com>
2025-11-04 16:33:28 -0800

    trouble update

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -35,32 +35,27 @@ return {
       cmd = "Trouble",
       keys = {
         {
-          "<leader>xx",
+          "<leader>tr",
           "<cmd>Trouble diagnostics toggle focus=true<cr>",
           desc = "Diagnostics (Trouble)",
         },
         {
-          "<leader>xX",
-          "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-          desc = "Buffer Diagnostics (Trouble)",
-        },
-        {
-          "<leader>cs",
+          "<leader>ts",
           "<cmd>Trouble symbols toggle focus=false<cr>",
           desc = "Symbols (Trouble)",
         },
         {
-          "<leader>cl",
+          "<leader>tl",
           "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
           desc = "LSP Definitions / references / ... (Trouble)",
         },
         {
-          "<leader>xL",
+          "<leader>tp",
           "<cmd>Trouble loclist toggle<cr>",
           desc = "Location List (Trouble)",
         },
         {
-          "<leader>xQ",
+          "<leader>tq",
           "<cmd>Trouble qflist toggle<cr>",
           desc = "Quickfix List (Trouble)",
         },
